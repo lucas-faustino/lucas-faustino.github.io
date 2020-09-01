ab340c4b1e32d01827494be40ba6841aa78bc29f
lucas-faustino <lucas.faustino1990@outlook.com>
2020-09-01 23:56:39 +0900

    first push

---
+++ precache-manifest.2be986fd7657594a83996c3a75275aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3254bb7949b44732f7f1e3c882bba013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ca39b936e25d5b2f5a6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a17bf967ac26a8d3feeb",
+    "url": "/static/js/2.5f887cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f887cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca39b936e25d5b2f5a6",
+    "url": "/static/js/main.d237c117.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
